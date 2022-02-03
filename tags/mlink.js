7db65cd4016f3b59d733cf15f86c2d6bbb4f28f9
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-02 18:35:19 -0700

    began building v2

---
+++ tags/mlink.js
@@ -0,0 +1,8 @@
+function destination1(){
+
+    document.getElementById("product1").innerHTML = ''
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("destinationlink1").href = ''
+    document.getElementById("redirect1").content = '5; url="https://author.borgcreative.com/"'
+
+}
