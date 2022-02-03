822604f49fefb89e66ab62c4ef152df0989b9456
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-02 18:56:24 -0700

    Rebuilt link pages
    -Rebuilt pages
    -Finished the removal of all third party ads
    -Added BCS Analytics
    -Implemented Content Controller

--- tags/mlink.js
+++ tags/mlink.js
@@ -2,7 +2,34 @@ function destination1(){
 
     document.getElementById("product1").innerHTML = ''
     document.getElementById("content1").innerHTML = ''
-    document.getElementById("destinationlink1").href = ''
-    document.getElementById("redirect1").content = '5; url="https://author.borgcreative.com/"'
+    document.getElementById("destinationlink1").href = 'https://author.borgcreative.com/?utm_source=mlink'
+
+
+    document.getElementById("ad1").src = 'https://source.borgcreative.com/bct/mk4-ad-pane-1.html'
+    document.getElementById("ad2").src = 'https://source.borgcreative.com/bct/mk4-ad-pane-3.html'
+
+}
+
+function destination2(){
+
+    document.getElementById("product1").innerHTML = ''
+    document.getElementById("content1").innerHTML = 'You viewing this page helps keep BCS Author free!'
+    document.getElementById("destinationlink1").href = 'https://author.borgcreative.com/?utm_source=mlink'
+
+
+    document.getElementById("ad1").src = 'https://source.borgcreative.com/bct/mk4-ad-pane-3.html'
+    document.getElementById("ad2").src = 'https://source.borgcreative.com/bct/mk4-ad-pane-1.html'
+
+}
+
+function destination3(){
+
+    document.getElementById("product1").innerHTML = ''
+    document.getElementById("content1").innerHTML = 'You viewing this page helps support the Fruity Web Browser!'
+    document.getElementById("destinationlink1").href = 'https://www.borgcreative.com/fruity.html?utm_source=mlink'
+
+
+    document.getElementById("ad1").src = 'https://source.borgcreative.com/bct/mk4-ad-pane-1.html'
+    document.getElementById("ad2").src = 'https://source.borgcreative.com/bct/mk4-ad-pane-3.html'
 
 }
